30280723a3364f04ac69ee3e98584a05e432261e
s3ns3i <eragonsm@gmail.com>
2019-01-29 01:21:28 +0100

    Receiver working with buzzer but without button

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -2,7 +2,6 @@
 
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
-#include <EEPROM.h>
 
 #define TOO_HOT "HOT"
 #define TOO_COLD "COL"
@@ -14,20 +13,23 @@ RH_ASK driver;
 
 uint8_t buf[13];
 uint8_t buflen;
-int addr = 0;
+uint8_t key[4];
+uint8_t value[8];
 
 void setup()
 {
-  pinMode(A1, OUTPUT);
+  pinMode(A0, OUTPUT);
   Serial.begin(9600); // Debugging only
   if (!driver.init())
+  {
     Serial.println("init failed");
+  }
 }
 
 void loop()
 {
-  // tone(A1, 1000, 2000);
   receiveData();
+  interpretSignal();
 }
 
 void receiveData()
@@ -39,60 +41,45 @@ void receiveData()
   }
   if (driver.recv(buf, &buflen)) // Non-blocking
   {
-    uint8_t key[4];
-    uint8_t value[8];
     readData(buf, key, value);
-    Serial.print("Key: ");
-    Serial.println((char *)key);
-    Serial.print("Value: ");
-    Serial.println((char *)value);
+  }
+}
+
+void interpretSignal()
+{
     if (strcmp(TOO_COLD, (char *)key) == 0)
     {
       Serial.print("Alert!: ");
-      saveData(buf);
+      Serial.println((char *)buf);
+      tone(A0, 500, 500);
+      delay(1000);
     }
     else if (strcmp(TOO_HOT, (char *)key) == 0)
     {
       Serial.print("Alert!: ");
       Serial.println((char *)buf);
-      saveData(buf);
+      tone(A0, 1000, 150);
+      delay(300);
     }
     else if (strcmp(OK, (char *)key) == 0)
     {
       // Message with a good checksum received, dump it.
       Serial.print("Message: ");
       Serial.println((char *)buf);
+      delay(900);
     }
-  }
-}
-
-void saveData(uint8_t *data)
-{
-  Serial.println("Saving data to EEPROM");
-  uint8_t key[4];
-  uint8_t value[8];
-  readData(data, key, value);
-  Serial.print("Int value: ");
-  Serial.println((char *)value);
-  EEPROM.write(TOO_HOT_ADDR, atoi((char *)value));
-  int readValue = EEPROM.read(TOO_HOT_ADDR);
-  Serial.print("Read value: ");
-  Serial.println(readValue);
-  // uint8_t readValue = 
 }
 
 void readData(const uint8_t *data, uint8_t *key, uint8_t *value)
 {
-  Serial.println("Reading data");
-  // char _key[4];
-  // char _value[8];
+  // Serial.println("Reading data");
   int i = 0;
   int j = 0;
   
   while(data[i] != ':')
   {
-    Serial.print("Reading key: ");
-    Serial.println(data[i]);
+    // Serial.print("Reading key: ");
+    // Serial.println(data[i]);
     key[i] = data[i];
     ++i;
   }
@@ -102,20 +89,13 @@ void readData(const uint8_t *data, uint8_t *key, uint8_t *value)
   
   while(data[i] != '\0')
   {
-    Serial.print("Reading value: ");
-    Serial.println(data[i]);
+    // Serial.print("Reading value: ");
+    // Serial.println(data[i]);
     value[j] = data[i];
     ++i;
     ++j;
   }
   value[j] = '\0';
-  
-  // key = _key;
-  // value = _value;
-  // for(int i = 0; data[i] != '\0'; i++)
-  // {
-  //   _key[i] = 
-  // }
 }
 
 /*
